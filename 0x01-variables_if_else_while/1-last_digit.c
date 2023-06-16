fd484b4b1ad2bfbeb3f6f26156d9f8a28f3de165
Nabemh <nhanior@gmail.com>
2023-06-16 13:17:28 -0700

    fixing whitespaces

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  *main -> assign a random number to the variable n each time it is executed
  *
  * Return: Always 0
  *  
  */
-
 int main(void)
 {
 	int n;
