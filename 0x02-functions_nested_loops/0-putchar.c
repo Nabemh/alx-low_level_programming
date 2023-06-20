426fe429edaa8e92f8ffe6d39dc92642fce3803c
Nabemh <nhanior@gmail.com>
2023-06-20 08:54:12 -0700

    test 2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include main.h
 
 /**
  *
@@ -9,7 +9,18 @@
  */
 
 int main (void)
+
 {
-	printf("_putchar\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t')'
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
+
+
 }
