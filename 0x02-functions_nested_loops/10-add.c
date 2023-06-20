816e65a81facd79e77bff3cf9e350bb8c4ff66da
Nabemh <nhanior@gmail.com>
2023-06-20 13:03:03 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main: adds two integers and returns the result
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	int num1 = 89;
+	int num2 = 9;
+
+	int n = num1 + num2;
+	_putchar("%d\n", n);
+	return (0);
+}
