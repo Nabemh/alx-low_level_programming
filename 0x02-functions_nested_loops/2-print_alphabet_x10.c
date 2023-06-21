3471a5613b7e9075cb6fdd4a775669ad69eeddf9
Nabemh <nhanior@gmail.com>
2023-06-20 18:46:36 -0700

    trail4

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,8 +5,9 @@
  *
  * Return: Always 0.
  */
-void print_alphabet_x10(void)
+int main(void)	
 {
+	print_alphabet_x10();
 	int letter;
 	int i;
 
@@ -18,4 +19,5 @@ void print_alphabet_x10(void)
 	       }
 	}
 	_putchar("\n");
+	return (0);
 }
