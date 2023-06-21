cc3f01f46165789f5bd5d61daa639c82143113fb
Nabemh <nhanior@gmail.com>
2023-06-20 18:17:53 -0700

    print alphabet x10 trial

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,16 @@
 /**
  * void: Entry point
  * Description: Print 10 times the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
  */
 
 void print_alphabet_x10(void)
 {
 	char alphabet;
-	int i = 0;
+	char letter;
 
-	while(i < 10; i++)
+	for(letter =1; letter <= 10; letter++)
 	{
 	       for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	       {
