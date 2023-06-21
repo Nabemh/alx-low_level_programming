37ad41a70ea020e12573648e666abb51c81a6b63
Nabemh <nhanior@gmail.com>
2023-06-20 19:00:17 -0700

    trial 5

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,9 +5,8 @@
  *
  * Return: Always 0.
  */
-int main(void)	
+void print_alphabet_x10(void)	
 {
-	print_alphabet_x10(){
 	int letter;
 	int i;
 
@@ -19,5 +18,5 @@ int main(void)
 	       }
 	}
 	_putchar("\n");
-	return (0);}
+	
 }
