5c3e2f583403311399807092a955b28ee7917e5c
Nabemh <nhanior@gmail.com>
2023-06-27 16:01:14 +0100

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - prints elemets of an array
+ * @a: prints elements of an array
+ * @n: number of elements to be printed
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+		printf("\n");
+}
