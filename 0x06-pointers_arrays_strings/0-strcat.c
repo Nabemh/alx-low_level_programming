b6ce78d5cd0d8b2aecaedda210b94d2e6c0aff44
Nabemh <nhanior@gmail.com>
2023-06-30 00:39:41 +0100

    trials 1 through 102(advanced tasks)

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,10 +12,13 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	i = 0;
+	j = 0;
 
 	while (dest[1] != '\0')
+	{
 		i++;
-	j = 0;
+	}
+	
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
+char *string_toupper(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] >= 'a' && s[a] <= 'z')
+			s[a] = s[a] - 32;
+	}
+
+	return (s);
+}
