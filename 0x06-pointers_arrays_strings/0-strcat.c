41b35dadce76d5b562428d6c772d7a8d2a0a7031
Nabemh <nhanior@gmail.com>
2023-06-30 00:04:16 +0100

    0-strcat.c test 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - appends a string to another
+ * @src: string to append
+ * @dest: string to append to
+ *
+ * Return: a pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[1] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
