7fb5c1eead62f0a0eeec00246f7a3816cdfea251
Nabemh <nhanior@gmail.com>
2023-07-06 10:53:35 +0100

    0 through 7 and 101 trial 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to be printed
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
