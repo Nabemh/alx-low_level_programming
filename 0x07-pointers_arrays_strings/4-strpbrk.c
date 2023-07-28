c35b9c6027b4240d08af58dc246d7635e59375d5
Nabemh <nhanior@gmail.com>
2023-07-28 21:51:54 +0100

    renamed dir

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,7 @@
 /**
  * *_strpbrk - searches a string for any of a set of bytes
  * @s: string to search
- * @accept: stringcontaining the bytes to look for
+ * @accept: string containing the bytes to look for
  *
  * Return: pointer to the byte in s that matches one of the bytes in accept
  * or NULL if no such byte is found
