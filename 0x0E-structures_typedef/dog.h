333c48e4bbcfab6304c0b894647ff152f244b9d3
Nabemh <nhanior@gmail.com>
2023-09-11 20:44:27 +0100

    task 0 - trail 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,25 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
 #define DOG(name, age, owner)
 	struct dog {
 		char *name;
 		float age;
 		char *owner;
 	};
+
+/**
+ * dog_t - typedef for struct dog
+ */
 
 typedef struct dog dog_t;
 
