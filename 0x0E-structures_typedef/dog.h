7f6cc79206cfb55a18a15f691677b64a46d91d90
Nabemh <nhanior@gmail.com>
2023-09-11 20:47:35 +0100

    task 3 trial 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,18 +9,16 @@
  *
  * Description: Longer description
  */
-
-#define DOG(name, age, owner)
-	struct dog {
-		char *name;
-		float age;
-		char *owner;
-	};
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 /**
  * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
