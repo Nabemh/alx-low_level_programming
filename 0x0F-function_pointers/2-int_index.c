d8a92c3b19015673982a7f1e2d174dca8e1ab39c
Nabemh <nhanior@gmail.com>
2023-09-13 18:08:07 +0100

    betty issues resolved

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,7 +4,7 @@
  * int_index - searches for an integer
  * @array: array to go through
  * @size: number of elements in array
- * @cmp: pointer to the function to be used 
+ * @cmp: pointer to the function to be used
  *
  * Return: -1 if no element, size <= 0
  */
@@ -13,16 +13,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if(array == NULL || cmp == NULL || size <= 0){
-		return -1;
+	if (array == NULL || cmp == NULL || size <= 0)
+	{
+		return (-1);
 	}
 
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		if(cmp(array[i]))
+		if (cmp(array[i]))
 		{
-			return i;
+			return (i);
 		}
 	}
 	return (-1);
