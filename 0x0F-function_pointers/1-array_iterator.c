ceb23f16ca4b6b1498382dd671a5487dba3c628f
Nabemh <nhanior@gmail.com>
2023-09-13 18:01:20 +0100

    projects 2 - the end - trial one

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function
+ * @array: array to be used
+ * @size: size of array
+ * @action: pointer to the function to be used
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if(array == NULL || action == NULL)
+	{
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	action(array[i]);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to go through
+ * @size: number of elements in array
+ * @cmp: pointer to the function to be used 
+ *
+ * Return: -1 if no element, size <= 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if(array == NULL || cmp == NULL || size <= 0){
+		return -1;
+	}
+
+
+	for(i = 0; i < size; i++)
+	{
+		if(cmp(array[i]))
+		{
+			return i;
+		}
+	}
+	return (-1);
+}
