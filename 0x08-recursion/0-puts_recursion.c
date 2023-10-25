b30af92f15a2d5b4a3ad617e5e5ed7e76b70e9ae
Nabemh <nhanior@gmail.com>
2023-10-25 14:24:40 +0100

    trial 2

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,11 @@ a#include "main.h"
 
 void _puts_recursion(char *s)
 {
-	if (*s != NULL)
+	if (*s != '\0')
 	{
 
 		_putchar(*s);
-		_puts_recursion(s++);
+		_puts_recursion(++s);
 	}
 	else
 		_putchar('\n');
