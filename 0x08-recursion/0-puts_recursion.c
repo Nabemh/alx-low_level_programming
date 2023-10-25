3a3507c119ac1575a898332e623a6afee755314e
Nabemh <nhanior@gmail.com>
2023-10-25 14:19:15 +0100

    task 0-101

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+a#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != NULL)
+	{
+
+		_putchar(*s);
+		_puts_recursion(s++);
+	}
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string whose lenght is being returned
+ *
+ * Return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == NULL)
+		return (0);
+
+	return (1 + _strlen_recursion(++s));
+}
+
