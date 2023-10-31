03ad27b1d3deaab7297bac68dd5a2db418711a89
Nabemh <nhanior@gmail.com>
2023-10-31 16:22:44 +0100

    task one done

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_array - creates and array of chars
+ * @size: size of array
+ * @c: characters to be added
+ *
+ * Return: NULL if error, pointer to array if success
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int n;
+
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = (char *) malloc(size * sizeof(char));
+	
+	if (ptr == NULL)
+	{
+		return (0);
+	}
+
+	while (n < size)
+	{
+		ptr[n] = c;
+		n++;
+	}
+	
+	return (ptr);
+}
