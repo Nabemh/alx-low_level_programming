1d3fc117d0c6cddf52197a83e240b0abac79cd15
Nabemh <nhanior@gmail.com>
2024-01-11 13:18:48 +0100

    trial 2

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,12 +11,10 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
 
-	while (h != NULL)
+	for (i = 0; h != NULL; i++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-
-		i++;
 	}
 
 	return (i);
