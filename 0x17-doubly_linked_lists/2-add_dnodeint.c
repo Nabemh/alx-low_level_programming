f2b2a76ee4761dd78ba0fbee0aab64551e51b30a
Nabemh <nhanior@gmail.com>
2024-01-12 10:07:10 +0100

    trail 3

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -28,6 +28,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->next = *head;
 	*head = new;
 	if (new->next != NULL)
-		(new->next)-> = new;
+		(new->next)->prev = new;
 	return (new);
 }
